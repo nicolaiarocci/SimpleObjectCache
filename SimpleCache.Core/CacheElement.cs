cd297a4e52b76418819808aab6691d1e34c44365
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-10-12 11:34:15 +0200

    Make sure Expiration field is also indexed in the db

--- SimpleCache.Core/CacheElement.cs
+++ SimpleCache.Core/CacheElement.cs
@@ -10,6 +10,7 @@ namespace Amica.vNext.SimpleCache
 	[Indexed]
 	public string TypeName { get; set; }
         public byte[] Value { get; set; }
+	[Indexed]
 	public DateTime? Expiration { get; set; }
 	public DateTimeOffset CreatedAt { get; set; }
     }
