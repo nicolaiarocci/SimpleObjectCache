67083a43cf71a0fb95ffda977243be456577842f
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-07-07 14:49:33 +0200

    Add a CommonAssemblyInfo.cs file, holding all projects Properties

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("SimpleCache")]
+[assembly: AssemblyDescription("Simple permanent, cross-platform and asynchronous key-value object cache")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nicola Iarocci, CIR 2000")]
+[assembly: AssemblyProduct("SimpleCache")]
+[assembly: AssemblyCopyright("Copyright © CIR 2000 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
