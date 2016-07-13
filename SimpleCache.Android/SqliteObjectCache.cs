34afd9d1347a0f4f0e434ff89bf0a817232b3f68
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-07-13 15:12:33 +0200

    Add Android support

---
+++ SimpleCache.Android/SqliteObjectCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using SQLite.Net;
+using SQLite.Net.Async;
+using SimpleCache;
+
+
+namespace SimpleCache
+{
+	public class SqliteObjectCache : SqliteObjectCacheBase
+	{
+		protected override SQLiteAsyncConnection PlatformConnection()
+		{
+			var dbPath = Path.Combine(
+				Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+				Path.Combine(ApplicationName, "SimpleCache"));
+
+			Directory.CreateDirectory(dbPath);
+
+			var lockedConnection = new SQLiteConnectionWithLock(
+				new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid(),
+				new SQLiteConnectionString(
+					Path.Combine(dbPath, "cache.db3"),
+					true));
+
+			return new SQLiteAsyncConnection(() => lockedConnection);
+		}
+	}
+}
